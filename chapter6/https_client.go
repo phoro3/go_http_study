f1881cce1248f4d01814d3f8118d7bcfe0c4e51e
phoro3 <puhro20@gmail.com>
2018-01-06 18:40:05 +0900

    Simple https client (page.168)

---
+++ chapter6/https_client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	//Read certificate
+	cert, err := ioutil.ReadFile("ca.crt")
+	if err != nil {
+		panic(err)
+	}
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(cert)
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	//Make Client
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	resp, err := client.Get("https://localhost:18443")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(string(dump))
+}
