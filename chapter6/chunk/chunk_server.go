d9ffcbeb3b2ff6332ea6a161955346c794d3b3e8
phoro3 <puhro20@gmail.com>
2018-01-07 22:47:57 +0900

    Chunk (page.178, 179)

---
+++ chapter6/chunk/chunk_server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>hello</body></html>")
+}
+
+func handlerChunkedResponse(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		panic("expected http.ResponseWriter to be an http.Flusher")
+	}
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(w, "Chunk #%d\n", i)
+		flusher.Flush()
+		time.Sleep(500 * time.Millisecond)
+	}
+	flusher.Flush()
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/chunked", handlerChunkedResponse)
+	log.Println("start http listening :18888")
+	err := http.ListenAndServe(":18888", nil)
+	log.Println(err)
+}
